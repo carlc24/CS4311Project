4c089630e122790adcd9d8cb0c708de570f710d5
z00210312 <39165880+z00210312@users.noreply.github.com>
2018-05-10 12:46:03 -0700

    Add files via upload

---
+++ hw3.TicTacToe.Prototype/src/Control/GameAttribute.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Control;
+
+import java.util.List;
+
+/**
+ *
+ * @author verastu
+ */
+public class GameAttribute {
+    List<String> atbInfo;
+    String player1;
+    String player2;
+    String defDesc;
+}
